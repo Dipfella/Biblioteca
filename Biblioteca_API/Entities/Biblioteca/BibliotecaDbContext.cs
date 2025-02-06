9e610285354581ce15bb5cd68e8a548739b748fb
Julian Sarmiento <julian.sarmiento.jeso@gmail.com>
2025-02-06 10:12:44 -0500

    Add Entities and configuration DbContext and Conecction to DataBase

---
+++ Biblioteca_API/Entities/Biblioteca/BibliotecaDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca_API.Entities.Biblioteca
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext()
+        {
+        }
+
+        public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Ejemplares> Ejemplares { get; set; }
+        public virtual DbSet<Libros> Libros { get; set; }
+        public virtual DbSet<Prestamos> Prestamos { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringFromConfig());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("SQL_Latin1_General_CP850_CI_AI");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static string GetStringFromConfig()
+        {
+            string filePath = "appsettings.json";
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile(filePath)
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            if (connectionString == null)
+            {
+                throw new InvalidOperationException("La cadena de conexión 'DefaultConnection' no está configurada.");
+            }
+            return connectionString;
+        }
+    }
---
+++ Biblioteca_API/Entities/Biblioteca/Usuarios.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_API.Entities.Biblioteca
+{
+    [Table("Usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        [Column("Id")]
+        public long Id { get; set; }
+
+        [Column("Nombre")]
+        public string Nombre { get; set; } = null!;
+
+        [Column("Apellido")]
+        public string Apellido { get; set; } = null!;
+
+        [Column("NumeroIdentificacion")]
+        public string NumeroIdentificacion { get; set; } = null!;
+
+        [Column("Telefono")]
+        public string? Telefono { get; set; }
+
+        [Column("Correo")]
+        public string Correo { get; set; } = null!;
+
+        [Column("FechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
